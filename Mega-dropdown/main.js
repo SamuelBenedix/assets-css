3b104a83b7631d623e82efc8f86441d7f02e4ae8
samuelbenedix <samuel1600.ss@gmail.com>
2021-02-11 16:15:02 +0700

    search_bar

---
+++ Mega-dropdown/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+	$("#search_input_box").hide();
+	$("#search").on("click", function () {
+		$("#search_input_box").slideToggle();
+		$("#search_input").focus();
+	});
+	$("#close_search").on("click", function () {
+		$("#search_input_box").slideUp(500);
+	});
+
+
+})
