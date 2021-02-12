9c01730851e7758a542c19f272fbe27eb14275b6
samuelbenedix <samuel1600.ss@gmail.com>
2021-02-12 16:37:32 +0700

    responsive-navbar

--- Mega-dropdown/main.js
+++ Mega-dropdown/main.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
 	});
 
 
-		/*==================================================================
+/*==================================================================
     [ Cart ]*/
 	$("#show-cart").on("click", function () {
 		$(".panel-cart").addClass("show-header-cart");
@@ -19,6 +19,25 @@ $(document).ready(function () {
 	$("#hide-cart").on("click", function () {
 		$(".panel-cart").removeClass("show-header-cart");
 	});
+
+	$('.dropdown').on("click",function () {
+		if($(this).children('.dropdown-mega-area').css("display") == "block" ){
+			$(this).children('.dropdown-mega-area').css("display","none")
+			$(".nav-links").css("overflow","hidden")
+		}else {
+			$(this).children('.dropdown-mega-area').css("display","block")
+			$(".nav-links").css("overflow","scroll")
+		}
+	})
+
+	$("#show-sidebar").on("click", function () {
+		$(".link-wrapper").addClass("show-sidebar");
+	});
+
+	$("#close-sidebar").on("click", function () {
+		$(".link-wrapper").removeClass("show-sidebar");
+	});
+
 
 
 })
