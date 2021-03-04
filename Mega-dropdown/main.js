7557225cde2b3e6f3ab540c4adca46bc3e1a0e4e
samuelbenedix <samuel1600.ss@gmail.com>
2021-03-04 13:19:19 +0700

    add prettier

--- Mega-dropdown/main.js
+++ Mega-dropdown/main.js
@@ -1,42 +1,41 @@
 $(document).ready(function () {
+  $("#search_input_box").hide();
+  $("#search").on("click", function () {
+    $("#search_input_box").slideToggle();
+    $("#search_input").focus();
+  });
+  $("#close_search").on("click", function () {
+    $("#search_input_box").slideUp(500);
+  });
 
-	$("#search_input_box").hide();
-	$("#search").on("click", function () {
-		$("#search_input_box").slideToggle();
-		$("#search_input").focus();
-	});
-	$("#close_search").on("click", function () {
-		$("#search_input_box").slideUp(500);
-	});
+  $("#show-cart").on("click", function () {
+    $(".panel-cart").addClass("show-header-cart");
+  });
 
-	$("#show-cart").on("click", function () {
-		$(".panel-cart").addClass("show-header-cart");
-	});
+  $("#hide-cart").on("click", function () {
+    $(".panel-cart").removeClass("show-header-cart");
+  });
 
-	$("#hide-cart").on("click", function () {
-		$(".panel-cart").removeClass("show-header-cart");
-	});
+  $(".megadropdown").on("click", function () {
+    if (
+      $(this).children(".dropdown-mega-area").css("display") == "none" &&
+      $(window).width() < 970
+    ) {
+      $(this).children(".dropdown-mega-area").css("display", "block");
+      $(".nav-links").css("overflow", "scroll");
+    } else {
+      if ($(window).width() < 970) {
+        $(this).children(".dropdown-mega-area").css("display", "none");
+        $(".nav-links").css("overflow", "hidden");
+      }
+    }
+  });
 
-	$('.dropdown').on("click",function () {
-		if($(this).children('.dropdown-mega-area').css("display") == "none" && $(window).width() < 970 ){
-			$(this).children('.dropdown-mega-area').css("display","block")
-			$(".nav-links").css("overflow","scroll")
-		}else {
-			if($(window).width() < 970){
-				$(this).children('.dropdown-mega-area').css("display","none")
-				$(".nav-links").css("overflow","hidden")
-			}	
-		}
-	})
+  $("#show-sidebar").on("click", function () {
+    $(".link-wrapper").addClass("show-sidebar");
+  });
 
-	$("#show-sidebar").on("click", function () {
-		$(".link-wrapper").addClass("show-sidebar");
-	});
-
-	$("#close-sidebar").on("click", function () {
-		$(".link-wrapper").removeClass("show-sidebar");
-	});
-
-
-
-})
+  $("#close-sidebar").on("click", function () {
+    $(".link-wrapper").removeClass("show-sidebar");
+  });
+});
